9ecbed3a0f01929cb62e410b6854770a4f8d1f7d
thejonroberts <jon.roberts.8@gmail.com>
2017-06-09 15:13:28 -0500

    tree object declared inside submit event and passed along in arguments.  I think.

--- main.js
+++ main.js
@@ -2,34 +2,33 @@
 let userHeight = document.getElementById("treeHeight");
 let userCharacter = document.getElementById("treeCharacter");
 let growButton = document.getElementById("growButton");
-let treeProperties = {
-	height: 0,
-	character: ""
-};
+
 //values grabbed from DOM and tree function called on click/submit
 growButton.addEventListener("click", function() {
+	let treeProperties = {
+		height   : 0,
+		character: ""
+	};
 	treeProperties.height = userHeight.value;
 	treeProperties.character = userCharacter.value;
-	if (inputFieldCheck() && lopsidedCheck()) {
-		tree(treeProperties);
-	}
-})
+	tree(treeProperties);
+	})
 //check that the user has entered values in both fields
-function inputFieldCheck() {
-	if ((treeProperties.height === "null") || (treeProperties.height === "" )) {
-		alert("Please enter a number for the tree height");
-	} else if (treeProperties.height < 1){
+function inputFieldCheck(tree) {
+	if ((tree.height === "null") || (tree.height === "" )) {
+		alert("Both fields must have a value!");
+	} else if (tree.height < 1){
 			alert("Please enter a positive integer (1, 2, 3, ...).  Our trees can only grow up.");
-			userCharacter.value = null;
-		} else if((treeProperties.character == "null") || (treeProperties.character == "" )) {
-				alert("Please enter a character for your tree");
+			userHeight.value = null;
+		} else if((tree.character === "null") || (tree.character === "" )) {
+				alert("Both fields must have a value");
 			} else return true;
 }
 //check if user has entered more than one character, confirm they want to do that, or cancel and reset char field.
-function lopsidedCheck() {
-	if (treeProperties.character.length === 1) {
+function lopsidedCheck(tree) {
+	if (tree.character.length === 1) {
 		return true;
-	} else if (treeProperties.character.length > 1) {
+	} else if (tree.character.length > 1) {
 		 if (confirm("Use only one character, or you will have a lopsided tree! Are you sure that's what you want?")) {
 		 	return true;
 		 	} else { 
@@ -37,21 +36,20 @@ function lopsidedCheck() {
 		 		return false;
 				}}
 }
-
 //determine how many of each character type should appear on each line.
 function numCharactersPerLine(i) {
 	let numCharacters = (1 + (2 * i));
 	return numCharacters;
 }
-function numSpacesPerLine(i) {
+function numSpacesPerLine(i, tree) {
 	let spacesEachSide = 
-	(treeProperties.height - 1 - i);
+	(tree.height - 1 - i);
 	return spacesEachSide;
 }
 //concatenate that number of chars/spaces to build line section
-function buildSpaceString(j) {
+function buildSpaceString(j, tree) {
 	let spaceString = "";
-	for (var i = 0; i < numSpacesPerLine(j); i++) {
+	for (var i = 0; i < numSpacesPerLine(j, tree); i++) {
 		spaceString += " ";
 	}
 	return spaceString;
@@ -64,8 +62,9 @@ function buildCharacterString(j, char) {
 	return characterString;
 }
 //output each line to console until height reached.
-function tree(treeObject) {
-	for (var i = 0; i < treeObject.height; i++) {
-		console.log(buildSpaceString(i), buildCharacterString(i, treeObject.character));
-	}
+function tree(tree) {
+	if (inputFieldCheck(tree) && lopsidedCheck(tree))  {
+	for (var i = 0; i < tree.height; i++) {
+		console.log(buildSpaceString(i, tree), buildCharacterString(i, tree.character));
+	}}
 }
